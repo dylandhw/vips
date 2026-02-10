f545c1d32fa14f61d8d73cc2cacbed6632379b36
dylandhw <dylanwilkins2308@gmail.com>
2026-02-10 03:52:08 -0500

    fixed a possible memory leak

--- grid.go
+++ grid.go
@@ -46,7 +46,6 @@ func PartitionImage() {
 			}
 
 			cell := frame.Region(image.Rect(x1, y1, x2, y2))
-			defer cell.Close()
 
 			// with image write = ~1.11ms, without = ~3.8µs
 			//filename := fmt.Sprintf("cell_%d.jpg", count)
@@ -57,15 +56,16 @@ func PartitionImage() {
 			//
 			// let's eventually parallelize our buffers... we will work on that tomorrow. should speed things up significantly!
 			buffer, err := gocv.IMEncode(".jpg", cell)
+			cell.Close()
 
 			if err != nil {
 				fmt.Printf("trouble loading cell into buffer")
 			}
 
-			defer buffer.Close()
-
 			cellBuffer = bytes.NewBuffer(buffer.GetBytes())
-			fmt.Printf(">> Buffer size: %d bytes\n", cellBuffer.Len())
+			buffer.Close()
+
+			fmt.Printf("Buffer size: %d bytes\n", cellBuffer.Len())
 
 			count++
 		}
